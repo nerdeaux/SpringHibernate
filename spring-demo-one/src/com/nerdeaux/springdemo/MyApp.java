710c1fd9a05e83527a2852da583c5893de716ede
Vusi <vusi.sizani@gmail.com>
2020-08-11 12:28:27 +0200

    my first commit

---
+++ spring-demo-one/src/com/nerdeaux/springdemo/MyApp.java
@@ -0,0 +1,14 @@
+package com.nerdeaux.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// create object
+		Coach theCoach = new TrackCoach();
+		
+		//use the object
+		System.out.println(theCoach.getDailyWorkout());
+
+	}
+
+}
---
+++ spring-demo-one/src/com/nerdeaux/springdemo/RugbyCoach.java
@@ -0,0 +1,10 @@
+package com.nerdeaux.springdemo;
+
+public class RugbyCoach implements Coach{
+	
+	@Override
+	public String getDailyWorkout(){
+		return "Spend 30 minutes on tackling practice.";	
+	}
+	
+}
